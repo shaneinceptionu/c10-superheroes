1af8ffb4b2871abc1f9e2ca5cd408bf27c8ec04f
Danielle Barker - Mac <caseybarker86@gmail.com>
2023-04-17 11:46:58 -0600

    created SuperheroList component

---
+++ frontend/src/components/SuperheroList.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export function SuperheroList() {
+  const [superheroList, setSuperheroList] = useState([]);
+  useEffect(() => {
+    async function getSuperheroes() {
+      const response = await fetch("/api/superhero");
+      const superheroData = await response.json();
+      setSuperheroList(superheroData);
+    }
+    getSuperheroes();
+  }, []);
+
+  return (
+    <section>
+      <ul>
+        {superheroList.map((superhero) => {
+          return (
+            <li>
+              <Link to={`/detail/${superhero._id}`}>{superhero.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
