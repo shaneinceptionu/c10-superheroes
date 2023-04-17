38d6c4a866f41cb9f7fe0839ec1ba09b3f455cf8
Danielle Barker - Mac <caseybarker86@gmail.com>
2023-04-17 11:48:13 -0600

    Separated parts of app into pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,81 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
-import { SuperheroDetail } from "./SuperheroDetail";
-function SuperheroForm() {
-  const [errorMessage, setErrorMessage] = useState();
-  const [name, setName] = useState("");
-  const [alterEgo, setAlterEgo] = useState("");
-  const [weakness, setWeakness] = useState("");
-  const [placeOfBirth, setPlaceOfBirth] = useState("");
-  const [superpower, setSuperpower] = useState("");
-  const superhero = {
-    name,
-    alterEgo,
-    weakness,
-    placeOfBirth,
-    superpower,
-  };
-  const submitSuperhero = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await fetch("/api/superhero", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(superhero),
-      });
-      if (!response.ok) {
-        console.log("bad thing happened");
-        const error = await response.json();
-        setErrorMessage(error.message);
-      } else {
-        const superheroData = await response.json();
-        console.log("response data is", superheroData);
-      }
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-  return (
-    <div>
-      <form onSubmit={submitSuperhero}>
-        <label>Superhero Name:</label>
-        <input onChange={(event) => setName(event.target.value)} />
-        <label>Alter Ego:</label>
-        <input onChange={(event) => setAlterEgo(event.target.value)} />
-        <label>Superpower:</label>
-        <input onChange={(event) => setSuperpower(event.target.value)} />
-        <label>Weakness:</label>
-        <input onChange={(event) => setWeakness(event.target.value)} />
-        <label>Place of Birth:</label>
-        <input onChange={(event) => setPlaceOfBirth(event.target.value)} />
-        <label>Age:</label>
-
-        <input type="submit" title="submit" />
-        <p style={{ color: "red" }}>{errorMessage}</p>
-      </form>
-    </div>
-  );
-}
+import { Link, Route, Routes } from "react-router-dom";
+import { SuperheroForm } from "./components/SuperheroForm";
+import { SuperheroList } from "./components/SuperheroList";
+import { SuperheroDetail } from "./components/SuperheroDetail";
 function App() {
-  const [superheroList, setSuperheroList] = useState([]);
-
-  useEffect(() => {
-    async function getSuperheroes() {
-      const response = await fetch("/api/superhero");
-      const superheroData = await response.json();
-      setSuperheroList(superheroData);
-    }
-    getSuperheroes();
-  }, []);
-
   return (
     <div>
-      {superheroList.map((superhero) => {
-        return <SuperheroDetail superhero={superhero} />;
-      })}
-      <SuperheroForm />
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/add-superhero">Add Superhero</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<SuperheroList />} />
+        <Route path="/detail/:id" element={<SuperheroDetail />} />
+        <Route path="/add-superhero" element={<SuperheroForm />} />
+      </Routes>
     </div>
   );
 }
